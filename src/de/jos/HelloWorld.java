75bc4dba52064fd4656be7d72b215f210dfaa175
Julian Osmer <julian.osmer@outlook.de>
2021-11-08 17:03:04 +0100

    update

--- src/de/jos/HelloWorld.java
+++ src/de/jos/HelloWorld.java
@@ -5,7 +5,11 @@ import org.itk.simple.*;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        var resultImage = SimpleITK.elastix(SimpleITK.readImage("fixedImage.png"), SimpleITK.readImage("movingImage.png"), "translation");
+        var resultImage = SimpleITK.elastix(
+                SimpleITK.readImage("sae/he_phh3/Bio5_HE.png", PixelIDValueEnum.sitkInt8),
+                SimpleITK.readImage("sae/he_phh3/Bio5_PHH3.png", PixelIDValueEnum.sitkInt8),
+                "translation"
+        );
 
         SimpleITK.show(resultImage, "Hello World: Python", true);
     }
